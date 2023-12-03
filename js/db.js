21e519e680de24742f948e607b5b2fc5a4b312f5
Isabela Gualberto <isabelagualberto7@gmail.com>
2023-12-03 16:55:31 -0400

    commit sla

--- js/db.js
+++ js/db.js
@@ -27,7 +27,6 @@ window.addEventListener('DOMContentLoaded', async event =>{
     document.getElementById('btnCadastro').addEventListener('click', adicionarLocalizacao);
     document.getElementById('btnCarregar').addEventListener('click', buscarTodasLocalizacoes);
     document.getElementById('btnRemover').addEventListener('click', removerLocalizacao);
-    document.getElementById('btnBuscar').addEventListener('click', buscarLocalizacao);
 });
 
 async function buscarTodasLocalizacoes(){
@@ -40,12 +39,12 @@ async function buscarTodasLocalizacoes(){
     if(localizacoes){
         const divLista = localizacoes.map(localizacao => {
             return `<div class="item">
-                    <p>Anotação</p>
-                    <p>${localizacao.titulo} - ${localizacao.data} </p>
+                    <p>STARBUCKS</p>
+                    <p>${localizacao.titulo}</p>
                     <p>${localizacao.pais}</p>
-                    <p>${localizacao.descricao}</p>
-                    <p>${localizacao.latitude}</p>
-                    <p>${localizacao.longitude}</p>
+                    <p>${localizacao.endereco}</p>
+                    <p>${localizacao.latitudee}</p>
+                    <p>${localizacao.longitudee}</p>
                    </div>`;
         });
         listagem(divLista.join(' '));
@@ -55,14 +54,13 @@ async function buscarTodasLocalizacoes(){
 async function adicionarLocalizacao() {
     let titulo = document.getElementById("titulo").value;
     let pais = document.getElementById("pais").value;
-    let descricao = document.getElementById("descricao").value;
-    let data = document.getElementById("data").value;
-    let latitude = document.getElementById("latitude").value;
-    let longitude = document.getElementById("longitude").value;
+    let endereco = document.getElementById("endereco").value;
+    let latitudee = document.getElementById("latitudee").value;
+    let longitudee = document.getElementById("longitudee").value;
     const tx = await db.transaction('localizacao', 'readwrite')
     const store = tx.objectStore('localizacao');
     try {
-        await store.add({ titulo: titulo, pais: pais, descricao: descricao, data: data, latitude: latitude, longitude: longitude });
+        await store.add({ titulo: titulo, pais: pais, endereco: endereco, latitudee: latitudee, longitudee: longitudee });
         await tx.done;
         limparCampos();
         console.log('Registro adicionado com sucesso!');
@@ -91,31 +89,6 @@ async function removerLocalizacao() {
         tx.abort();
     }
 }
-
-async function buscarLocalizacao() {
-    let busca = document.getElementById("busca").value;
-    const tx = await db.transaction('localizacao', 'readonly');
-    const store = tx.objectStore('localizacao');
-    const index = store.index(titulo);
-    const localizacoes = await index.getAll(IDBKeyRange.only(busca));
-    if (localizacoes.length > 0) {
-        const divLista = localizacoes.map(localizacao => {
-            return `<div class="item">
-            <p>Starbucks</p>
-            <p>Título: ${localizacao.titulo}</p>
-            <p>Descrição: ${localizacao.pais}</p>
-            <p>Descrição: ${localizacao.descricao}</p>
-            <p>Data de cadastro: ${localizacao.data}</p>
-            <p>Data de cadastro: ${localizacao.latitude}</p>
-            <p>Data de cadastro: ${localizacao.longitude}</p>
-            </div>`;
-        });
-        listagem(divLista.join(''));
-    } else {
-        listagem(`<p>Nenhuma localização encontrada com esse nome.</p>`);
-    }
-}
-
 
 function listagem(text){
     document.getElementById('resultados').innerHTML = text;
